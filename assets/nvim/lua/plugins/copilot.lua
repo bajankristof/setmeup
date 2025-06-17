91a6932ce9e05b7128da262e50b0843e36dcf3b9
bajankristof <bajankristof@gmail.com>
2025-06-17 15:06:12 +0200

    chore: update Copilot key bindings

--- assets/nvim/lua/plugins/copilot.lua
+++ assets/nvim/lua/plugins/copilot.lua
@@ -1,4 +1,11 @@
 return {
   'github/copilot.vim',
   event = { 'InsertEnter', 'VeryLazy' },
+  config = function()
+    vim.g.copilot_no_tab_map = true
+
+    vim.keymap.set('i', '<C-j>', 'copilot#Accept("")', { expr = true, replace_keycodes = false })
+    vim.keymap.set('i', '<C-l>', 'copilot#AcceptWord("")', { expr = true, replace_keycodes = false })
+    vim.keymap.set('i', '<C-n>', 'copilot#Dismiss()', { expr = true, replace_keycodes = false })
+  end,
 }
